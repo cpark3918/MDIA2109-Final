2ba15e27f49a61c1fa6d4fb56bc2a75bfba8acd4
cpark3918 <cindypark31@gmail.com>
2020-04-13 20:12:50 -0700

    finished tutorial

--- comps/paneltitle/index.js
+++ comps/paneltitle/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './paneltitle.css';
 
-const PanelTitle = ({text, color}) => <div 
+const PanelTitle = ({text, color, fontSize}) => <div 
 className="panel_title">
-    <h2 style={{color:color}}>{text}</h2>
+    <h2 style={{color:color, fontSize: fontSize}}>{text}</h2>
 </div> 
 
 PanelTitle.defaultProps = {
     text:"Default Panel Title",
     color: "#B7D4A0",
+    fontSize: "25pt"
 }
 
 export default PanelTitle;
