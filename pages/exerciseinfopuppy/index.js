ffe7264b37f51efede7dc50a64e8b3b187b1c9a0
virginiesw <virginiesw@gmail.com>
2020-04-20 19:43:03 -0700

    added semi transparent screen and hamburger menu

--- pages/exerciseinfopuppy/index.js
+++ pages/exerciseinfopuppy/index.js
@@ -9,22 +9,24 @@ import './exerciseinfopuppy.css';
 
 function clickExit() {
     if (true) {
-    Router.push("/maintenance");
+        Router.push("/maintenance");
     }
 }
 
-const ExerciseInfoPuppy = ({ backgroundColor}) => <div
+const ExerciseInfoPuppy = ({ backgroundColor }) => <div
     style={{ backgroundColor: backgroundColor }}
-    className="exerciseinfo">
+    className="exerciseinfo" id="mainappinfoscreen" >
     <div className="defaultscreen">
-        <div className="exercise-info_logo">
-            <LogoSmall />
-        </div>
-        <div className="exercise-info_page">
-            <Maintenance />
-        </div>
-        <div onClick={clickExit} className="exerciseinfoPanel">
-           <Link href="/maintenance"><InfoPanel title="Exercise" info="Puppies tend to have burst of energy throughout the day, also known as the zoomies. Because of that, it is better to give your puppy multiple shorter walks (5-10mins)  instead of one long walk. One long walk instead of multiple shorter ones can be too difficult on their body as it is still developing. It is especially important to remember for smaller breeds that your puppies legs are quite a bit shorter than yours! Stay on your puppies pace and allow them to rest if they start getting tired" /></Link> 
+        <div className="transparencyScreen">
+            <div className="exercise-info_logo">
+                <LogoSmall />
+            </div>
+            <div className="exercise-info_page">
+                <Maintenance />
+            </div>
+            <div onClick={clickExit} className="exerciseinfoPanel">
+                <Link href="/maintenance"><InfoPanel title="Exercise" info="Puppies tend to have burst of energy throughout the day, also known as the zoomies. Because of that, it is better to give your puppy multiple shorter walks (5-10mins)  instead of one long walk. One long walk instead of multiple shorter ones can be too difficult on their body as it is still developing. It is especially important to remember for smaller breeds that your puppies legs are quite a bit shorter than yours! Stay on your puppies pace and allow them to rest if they start getting tired" /></Link>
+            </div>
         </div>
     </div>
 </div>
