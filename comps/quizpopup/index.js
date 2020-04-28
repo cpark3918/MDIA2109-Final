af16cbd8f436a5a532430366b2ce4f528a2ab47e
= <virginiesw@gmail.com>
2020-04-28 11:30:23 -0700

    added font to quiz pop up

--- comps/quizpopup/index.js
+++ comps/quizpopup/index.js
@@ -8,7 +8,7 @@ const QuizPopup = () => <div className="quizpopup_contain">
     <img className="xButton" src={closeWindowGray} />
      <div className="quizpopup_box">
          <h1 id="title">Quiz Time!</h1>
-         <p>Time to put your <br></br>knowledge to the test!</p>
+         <p id="quizpopupp">Time to put your <br></br>knowledge to the test!</p>
          <CustomButton text="Start Quiz" fontSize="30pt"/>
      </div>
 </div>
