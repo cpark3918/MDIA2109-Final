f72f76373a6791e3d2aaf946ce38f9c01718b385
= <virginiesw@gmail.com>
2020-05-05 19:08:01 -0700

    fixing bugs for transition

--- pages/tutorial4/index.js
+++ pages/tutorial4/index.js
@@ -8,44 +8,46 @@ import PageTitle from '../../comps/pagetitle';
 
 const tapIcon = require('../../animations/tap.gif')
 
-function nextTutorial(){
+function nextTutorial() {
     document.querySelector("#page").style.left = "-100%";
-    setTimeout(function(){
+    setTimeout(function () {
         Router.push("/tutorial5");
     }, 600)
 }
 
 const Tutorial04 = () => {
-    
+
     useEffect(() => {
-        setTimeout(()=>{
+        setTimeout(() => {
             document.querySelector("#page").style.left = "0%";
         }, 50);
     }, []);
-    
-    return <div className="defaultscreen" id="page" >
+
+    return <div className="defaultscreen">
         <div className="tut">
             <div className="tut_icons">
                 <LogoSmall />
-                <PageTitle text="Tutorial" color="#000" fontSize="35pt"/>
+                <PageTitle text="Tutorial" color="#000" fontSize="35pt" />
             </div>
             <Menu />
-            <div className="tut_box" id="tut4_box" onClick={nextTutorial}>
-                <PanelTitle text="(Question)" color="#707070" />
-                <div className="tut_topics">
-                    <h2 id="maint">7 weeks old</h2>
-                    <h2 id="train">16 weeks old</h2>
-                    <h2 id="health">1 year old</h2>
-                    <img id="tut4_tap" src={tapIcon} />
+            <div id="page">
+                <div className="tut_box" id="tut4_box" onClick={nextTutorial}>
+                    <PanelTitle text="(Question)" color="#707070" />
+                    <div className="tut_topics">
+                        <h2 id="maint">7 weeks old</h2>
+                        <h2 id="train">16 weeks old</h2>
+                        <h2 id="health">1 year old</h2>
+                        <img id="tut4_tap" src={tapIcon} />
+                    </div>
+                    <p>Take the quiz (per category).<br></br>Each quiz consists of 3 questions!</p>
                 </div>
-                <p>Take the quiz (per category).<br></br>Each quiz consists of 3 questions!</p>
-            </div>
-            <div className="progress_box">
-                <div className="progress_inactive"></div>
-                <div className="progress_inactive"></div>
-                <div className="progress_inactive"></div>
-                <div className="progress_active"></div>
-                <div className="progress_inactive" onClick={nextTutorial}></div>
+                <div className="progress_box">
+                    <div className="progress_inactive"></div>
+                    <div className="progress_inactive"></div>
+                    <div className="progress_inactive"></div>
+                    <div className="progress_active"></div>
+                    <div className="progress_inactive" onClick={nextTutorial}></div>
+                </div>
             </div>
         </div>
     </div>
